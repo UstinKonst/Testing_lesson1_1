e51129a84dfc14497a0d5a2a636364c8d3fe4c03
UstinKonst <ustinovskyk@mail.ru>
2022-05-25 17:39:40 +0300

    1

---
+++ src/main/java/lesson1/HomeWorkApp.java
@@ -0,0 +1,50 @@
+package lesson1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 1;
+        int b = 2;
+
+        if (a + b >= 0) {
+            System.out.println("Summa polojitelna");
+        } else {
+            System.out.println("Summa otricatelna");
+        }
+    }
+
+    public static void printColor() {
+        int  value = 55;
+
+        if (value <= 0) {
+            System.out.println("red");
+        } else if(value > 100){
+            System.out.println("green");
+        } else {
+            System.out.println("yellow");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 1;
+        int b = 2;
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
